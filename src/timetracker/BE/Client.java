cd748cdf6449d5d467b8ee3b6049f6d7cb7e0035
René Jørgensen <56912824+LeChampDK@users.noreply.github.com>
2020-04-28 03:22:50 +0200

    BE

---
+++ src/timetracker/BE/Client.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timetracker.BE;
+
+/**
+ *
+ * @author René Jørgensen
+ */
+public class Client
+{
+    private int client_id;
+    private String client_name; 
+    private int default_rate;
+
+    public Client(int client_id, String client_name, int default_rate)
+    {
+        this.client_id = client_id;
+        this.client_name = client_name;
+        this.default_rate = default_rate;
+    }
+
+    public Client()
+    {
+    }
+
+    public int getClient_id()
+    {
+        return client_id;
+    }
+
+    public void setClient_id(int client_id)
+    {
+        this.client_id = client_id;
+    }
+
+    public String getClient_name()
+    {
+        return client_name;
+    }
+
+    public void setClient_name(String client_name)
+    {
+        this.client_name = client_name;
+    }
+
+    public int getDefault_rate()
+    {
+        return default_rate;
+    }
+
+    public void setDefault_rate(int default_rate)
+    {
+        this.default_rate = default_rate;
+    }
+    
+    
+}
