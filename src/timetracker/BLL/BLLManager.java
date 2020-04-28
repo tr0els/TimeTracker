510a1f01e1b96ee6c801efaf4ea42c3216ba1505
Guruen <56912822+Guruen@users.noreply.github.com>
2020-04-28 11:18:49 +0200

    Start/oprette/pause task

--- src/timetracker/BLL/BLLManager.java
+++ src/timetracker/BLL/BLLManager.java
@@ -5,6 +5,7 @@
  */
 package timetracker.BLL;
 
+import timetracker.BE.Task;
 import timetracker.DAL.DALException;
 import timetracker.DAL.DALManager;
 
@@ -32,4 +33,33 @@ public class BLLManager {
         dal.deleteProject(clientID, projectName, hourlyPay);
     }
     
+    /**
+     * opretter og starter en ny task
+     * @param task_name
+     * @param billable
+     * @param project_id
+     * @param person_id
+     * @return 
+     */
+    public Task createTask(String task_name, boolean billable, int project_id, int person_id)
+    {
+        return dal.createTask(task_name, billable, project_id, person_id);
+    }
+    
+    /**
+     * starter ny tidstagning på eksisterende task
+     * @param task_id 
+     */
+    public void startTask(int task_id){
+        dal.startTask(task_id);
+    }
+    
+    /**
+     * pauser/stopper en aktiv task
+     * @param task_id 
+     */
+    public void pauseTask(int task_id){
+        dal.pauseTask(task_id);
+        
+    }
 }
--- src/timetracker/DAL/DALManager.java
+++ src/timetracker/DAL/DALManager.java
@@ -7,6 +7,15 @@ package timetracker.DAL;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import timetracker.BE.Task;
 
 /**
  *
@@ -38,7 +47,7 @@ public class DALManager {
     }
 
     public void deleteProject(int clientID, String projectName, int hourlyPay) {
-                try ( Connection con = dbCon.getConnection()) {
+        try ( Connection con = dbCon.getConnection()) {
 
             String sql = "DELETE FROM Project WHERE Project_name = ? AND project_rate = ? AND client_id = ?";
 
@@ -54,4 +63,92 @@ public class DALManager {
         }
     }
 
+    /**
+     * opretter ny task og returnere task
+     *
+     * @param task_name
+     * @param billable
+     * @param project_id
+     * @param person_id
+     * @return
+     */
+    public Task createTask(String task_name, boolean billable, int project_id, int person_id) {
+        try ( Connection con = dbCon.getConnection()) {
+
+            Task task = null;
+
+            int int_billable = 0;//konvertere boolean til 0 el. 1
+            if (billable == true) {
+                int_billable = 1;
+            }
+
+            String sql = "INSERT INTO Task (task_name, billable, project_id, person_id) VALUES (?,?,?,?)";
+
+            PreparedStatement ps = con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, task_name);
+            ps.setInt(2, int_billable);
+            ps.setInt(3, project_id);
+            ps.setInt(4, person_id);
+
+            int affectedRows = ps.executeUpdate();
+
+            if (affectedRows == 1) {
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    int task_id = rs.getInt(1);
+
+                    startTask(task_id);
+
+                    task = new Task(task_id, task_name, billable, project_id, person_id);
+
+                }
+
+            }
+
+            return task;
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * opretter starttidspunkt for ny task
+     *
+     * @param task_id
+     */
+    public void startTask(int task_id) {
+        try ( Connection con = dbCon.getConnection()) {
+
+            String sql = "INSERT INTO Task_log (task_id, task_start) VALUES (?, CURRENT_TIMESTAMP)";
+
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setInt(1, task_id);
+
+        } catch (Exception e) {
+
+        }
+
+    }
+
+    /**
+     * indsætter pause/stop tidspunkt task på task_id hvor task_end ikke er sat endnu.
+     *
+     * @param task_id
+     */
+    public void pauseTask(int task_id) {
+        try ( Connection con = dbCon.getConnection()) {
+
+            String sql = "UPDATE Task_log SET task_end=CURRENT_TIMESTAMP WHERE task_id = task_id AND task_end = null";
+
+            PreparedStatement ps = con.prepareStatement(sql);
+
+
+        } catch (Exception e) {
+
+        }
+
+    }
 }
