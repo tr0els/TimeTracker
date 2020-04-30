2adb45dd03e091b583edc32fb711c5eb38e3c45d
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2020-04-30 10:39:27 +0200

    save

--- src/timetracker/GUI/Controller/MainController.java
+++ src/timetracker/GUI/Controller/MainController.java
@@ -5,7 +5,6 @@
  */
 package timetracker.GUI.Controller;
 
-
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDrawer;
 import java.io.IOException;
@@ -24,8 +23,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-   import timetracker.GUI.Model.TaskModel;
-
+import timetracker.GUI.Model.TaskModel;
 
 /**
  *
@@ -34,24 +32,16 @@ import javafx.stage.Stage;
  */
 public class MainController implements Initializable {
 
-    @FXML
-    private JFXButton adminbtb;
-    @FXML
-    private JFXButton user;
-    @FXML
-    private AnchorPane root;
-
-    
-     /**
-     * Singleton opsætning af vores MainController. singleton gør at vores maincontroller ikke vil
-     * blive instansieret mere end en gang.
+    /**
+     * Singleton opsætning af vores MainController. singleton gør at vores
+     * maincontroller ikke vil blive instansieret mere end en gang.
      */
     private static TaskModel model;
     private static MainController main = null;
 
     public MainController() throws DALException, SQLException {
         model = TaskModel.getInstance();
-    } 
+    }
 
     public static MainController getInstance() throws DALException, SQLException {
         if (main == null) {
@@ -59,114 +49,51 @@ public class MainController implements Initializable {
         }
         return main;
     }
- 
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
- 
- 
 
     }
 
+    public void handeladminlogin(ActionEvent event) throws IOException {
 
-   @FXML
-    private void handeladminlogin(ActionEvent event) throws IOException {
-        
-            
-            
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("/timetracker/GUI/View/Menubar.fxml"));
-            loader.load();
-            Parent root = loader.getRoot();
-            
-            //MenubarController controller = loader.getController();
-            
-            
-            Scene scene = new Scene(root);
-            Stage stage = new Stage();
-            stage.setTitle("Time Tracker");
-            Stage Currentstage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            Currentstage.close();
-            stage.setScene(scene);
-            stage.setResizable(false);
-            stage.show();
-        
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/timetracker/GUI/View/Menubar.fxml"));
+        loader.load();
+        Parent root = loader.getRoot();
+
+        //MenubarController controller = loader.getController();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setTitle("Time Tracker");
+        Stage Currentstage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Currentstage.close();
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+
     }
 
-    @FXML
-    private void handeluserlogin(ActionEvent event) throws IOException {
-        
-         
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("/timetracker/GUI/View/Menubar.fxml"));
-            loader.load();
-            Parent root = loader.getRoot();
-            
-            MenubarController controller = loader.getController();
-            controller.getBrugermanagerbtb().setVisible(false);
-            controller.getKlientmanagerbtb().setVisible(false);
-            controller.getProjektbtb().setVisible(false);
-            Scene scene = new Scene(root);
-            Stage stage = new Stage();
-            stage.setTitle("Time Tracker");
-            Stage Currentstage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            Currentstage.close();
-            stage.setScene(scene);
-            stage.setResizable(false);
-            stage.show();
-        
-        
+    public void handeluserlogin(ActionEvent event) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/timetracker/GUI/View/Menubar.fxml"));
+        loader.load();
+        Parent root = loader.getRoot();
+
+        MenubarController controller = loader.getController();
+        controller.getBrugermanagerbtb().setVisible(false);
+        controller.getKlientmanagerbtb().setVisible(false);
+        controller.getProjektbtb().setVisible(false);
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setTitle("Time Tracker");
+        Stage Currentstage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Currentstage.close();
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+
     }
 
-
-    
-    
-    
-
-
-    /**
-     * Tager det info som admin har puttet ind i "Projekt Manager" menuen og
-     * sender det ned for at blive gemt på serveren
-     *
-     * @throws DALException
-     */
-    public void createProject() throws DALException {
-        int clientID = 1;
-        String projectName = "projekt 9";
-        int hourlyPay = 200;
-
-        model.createProject(clientID, projectName, hourlyPay);
-    }
-
-    /**
-     * Tager det projekt som Admin har valgt i "projekt Manager" menuen og
-     * sender det ned til DAL så det kan fjernes fra serveren.
-     *
-     * @throws DALException
-     */
-    public void deleteProject() throws DALException {
-        int clientID = 1;
-        String projectName = "projekt 2";
-        int hourlyPay = 200;
-
-        model.deleteProject(clientID, projectName, hourlyPay);
-    }
-
-    /**
-     * Henter projectID fra det projekt som admin har valgt. Tager så det
-     * updateret data som er inputet og sender det ned til DAL så infoen på
-     * databasen kan updateres
-     *
-     * @throws DALException
-     */
-    public void editProject() throws DALException {
-        int clientID = 1;
-        String projectName = "projekt 2";
-        int hourlyPay = 300;
-        int projectID = 6;
-
-        model.editProject(clientID, projectName, hourlyPay, projectID);
-    }
-
-  
 }
-
--- src/timetracker/GUI/Controller/ProjektManagerAdminController.java
+++ src/timetracker/GUI/Controller/ProjektManagerAdminController.java
@@ -7,7 +7,16 @@ package timetracker.GUI.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXTreeTableView;
+import javafx.event.ActionEvent;
+import timetracker.DAL.DALException;
+import timetracker.GUI.Model.TaskModel;
 
 /**
  * FXML Controller class
@@ -16,6 +25,35 @@ import javafx.fxml.Initializable;
  */
 public class ProjektManagerAdminController implements Initializable {
 
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private AnchorPane viewholder;
+
+    @FXML
+    private JFXComboBox<?> combobox;
+
+    @FXML
+    private JFXTextField timepris;
+
+    @FXML
+    private JFXTextField projektnavn;
+
+    @FXML
+    private JFXTreeTableView<?> treeView;
+
+    @FXML
+    private JFXDrawer drawer;
+    
+        private static TaskModel model;
+
+    @FXML
+    void handleProjektAction(ActionEvent event) {
+
+    }
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +61,49 @@ public class ProjektManagerAdminController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+        /**
+     * Tager det info som admin har puttet ind i "Projekt Manager" menuen og
+     * sender det ned for at blive gemt på serveren
+     *
+     * @throws DALException
+     */
+    public void createProject() throws DALException {
+        int clientID = 1;
+        String projectName = "projekt 9";
+        int hourlyPay = 200;
+
+        model.createProject(clientID, projectName, hourlyPay);
+    }
+
+    /**
+     * Tager det projekt som Admin har valgt i "projekt Manager" menuen og
+     * sender det ned til DAL så det kan fjernes fra serveren.
+     *
+     * @throws DALException
+     */
+    public void deleteProject() throws DALException {
+        int clientID = 1;
+        String projectName = "projekt 2";
+        int hourlyPay = 200;
+
+        model.deleteProject(clientID, projectName, hourlyPay);
+    }
+
+    /**
+     * Henter projectID fra det projekt som admin har valgt. Tager så det
+     * updateret data som er inputet og sender det ned til DAL så infoen på
+     * databasen kan updateres
+     *
+     * @throws DALException
+     */
+    public void editProject() throws DALException {
+        int clientID = 1;
+        String projectName = "projekt 2";
+        int hourlyPay = 300;
+        int projectID = 6;
+
+        model.editProject(clientID, projectName, hourlyPay, projectID);
+    }
     
 }
