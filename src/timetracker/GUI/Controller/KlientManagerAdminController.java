f9017ef20baaa246fa1dbc6d9cebb3ba37946a95
CharlotteMajka <55383161+CharlotteMajka@users.noreply.github.com>
2020-05-04 11:19:06 +0200

    javadoc

--- src/timetracker/GUI/Controller/KlientManagerAdminController.java
+++ src/timetracker/GUI/Controller/KlientManagerAdminController.java
@@ -121,14 +121,23 @@ public class KlientManagerAdminController implements Initializable {
             Logger.getLogger(KlientManagerAdminController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    /**
+     * sætter alle klienterne ind i klient listviewet 
+     * @throws DALException
+     * @throws SQLException 
+     */
     private void populateClientList() throws DALException, SQLException {
  
        listviewfx.setItems(model.getClients());
 
     }
 
-
+    /**
+     * håndtere den valgte klient fra klient listviwet, og tranportere dens info til relevandte lables, og arktivere "Ret klient knappen" mm. 
+     * @param event
+     * @throws DALException
+     * @throws SQLException 
+     */
     @FXML
     private void getSelectedClient(MouseEvent event) throws DALException, SQLException {
 
@@ -136,13 +145,23 @@ public class KlientManagerAdminController implements Initializable {
        
         klientNavnlbl.setText(selectedClient.getClient_name());
         timeprislbl.setText(selectedClient.getDefault_rate() + " DKK");
+        
+        //gør ret klient btb og retvalgteklient btb synlige
         retklientbtb.setVisible(true);
         retvalgteklientnbtb.setVisible(true);
+        
+        //tilføjer projeketer til listviewet for den valgte klient
         addprojektstolistview(selectedClient);
+        
+        //resetter projekt info lablsne
         lblprojektnavn.setText("");
         lblprojekttimepris.setText("");
     }
-
+    
+    /**
+     * finder ud af hvilket tooltip der skal vises 
+     * @return 
+     */
     private Tooltip tooltipforprojektlist() {
             Tooltip tip = new Tooltip();
 
@@ -156,7 +175,11 @@ public class KlientManagerAdminController implements Initializable {
     return null;
     }
         
-        
+   /**
+    * hjælpe metode til at finde ud af hvad der er i projekt listviewet, og retunere en int
+    * som bruges i tooltipforprojektview() for at sætte den korrekte tooltip. 
+    * @return 
+    */     
     public int istheresomthinginprojectview(){
         
         if ( selectedClient == null) {
@@ -168,56 +191,68 @@ public class KlientManagerAdminController implements Initializable {
     }
 
       
-
+    /**
+     * tilføjer den valgte klients projekter til listviewet
+     * @param client
+     * @throws DALException
+     * @throws SQLException 
+     */
     private void addprojektstolistview(Client client) throws DALException, SQLException {
-
-        //selectedClient = listviewfx.getSelectionModel().getSelectedItem();
-
         listviewprojekts.setItems(FXCollections.observableArrayList(model.getClientprojcts(client)));
-        //listviewprojekts.setItems(FXCollections.observableArrayList(taskmodel.getProjects()));
     }
-
+    
+    /**
+     * Tooltip eventhandler, tjekker om musen hover over projektlistview for at sætte det rigtige tooltip
+     * @param event 
+     */
     @FXML
     private void tooltip(MouseEvent event) {
 
         listviewprojekts.setTooltip(tooltipforprojektlist());
     }
-
+    /**
+     * opret ny klient knap, som åbner draweren med txtinput felter til at oprette en klient
+     * @param event 
+     */
     @FXML
     private void showpanewithnewklient(ActionEvent event) {
         skuffe.setSidePane(opretnyklientpane);
         skuffe.open();
         skuffe.toFront();
     }
-
+    /**
+     * knap til at håndtere oprettelsen af en ny klien, knappen ligger i anchorpane, som ikke kan ses når draweren ikke er åben.
+     * @param event
+     * @throws DALException
+     * @throws SQLException 
+     */
     @FXML
     private void handleopretklient(ActionEvent event) throws DALException, SQLException {
-         for (int i = 0; i < model.getClients().size(); i++) {
-            System.out.println(i + "before");
-        }
-        
-         
-         
-        Client mockclient = new Client(5,"EASV",1234);
-        
-        
-        model.getClients().add(mockclient);
-        
-        for (int i = 0; i < model.getClients().size(); i++) {
-            System.out.println(i +"after");
-        }
+ 
+       Client newklient = new Client();
+       
+        String nytnavn = Nyklientnavn.getText().trim();
+        int timepris = Integer.parseInt(nydefaulttimepris.getText().trim());
+       
+        newklient = model.createClient(nytnavn, timepris);
+       
+        model.getClients().add(newklient);
 
-        
         listviewfx.refresh();
         skuffe.close();
 
     }
-
+    /**
+     * håndtere retklient, knappen ligger i anchorpane som kun kan ses når draweren med retklien er åben. 
+     * @param event
+     * @throws DALException
+     * @throws SQLException 
+     */
     @FXML
     private void handleretklient(ActionEvent event) throws DALException, SQLException {
        
         String retnavn = txtretnavn.getText().trim();
-        int rettimepris = Integer.parseInt(txtrettimepris.getText().trim());
+        int rettimepris = Integer.parseInt(txtrettimepris.getText().trim().replace(" DKK", " ").trim());
 
         selectedClient.setClient_name(retnavn);
         selectedClient.setDefault_rate(rettimepris);
@@ -226,24 +261,33 @@ public class KlientManagerAdminController implements Initializable {
         klientNavnlbl.setText(retnavn);
         listviewfx.refresh();
         skuffe.close();
+       
 
     }
-
+    /**
+     * åbner draweren med ret klien txtinputfelter
+     * @param event 
+     */
     @FXML
     private void openskuffemenretklient(ActionEvent event) {
         skuffe.setSidePane(retklientpane);
         skuffe.open();
         txtretnavn.setText(selectedClient.getClient_name());
-        txtrettimepris.setText(selectedClient.getDefault_rate() + "");
+        txtrettimepris.setText(selectedClient.getDefault_rate() + " DKK");
 
     }
-
+    /**
+     * sætter info lablesne men info om det valgte projekt. 
+     * @param event 
+     */
     @FXML
     private void getSelectedProjekt(MouseEvent event) {
        Project valgteprojekt = listviewprojekts.getSelectionModel().getSelectedItem();
        
+       
        lblprojektnavn.setText(valgteprojekt.getProject_name());
        lblprojekttimepris.setText(valgteprojekt.getProject_rate()+"DKK");
+       
        
     }
 
