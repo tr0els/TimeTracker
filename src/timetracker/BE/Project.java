45b23b5338b265554774285994aa69f07fbbac6c
Guruen <56912822+Guruen@users.noreply.github.com>
2020-05-12 19:00:03 +0200

    Update Project.java

--- src/timetracker/BE/Project.java
+++ src/timetracker/BE/Project.java
@@ -19,19 +19,31 @@ public class Project extends RecursiveTreeObject<Project>
     public String project_name; 
     private int project_rate;
     private int client_id;
+    private String total_tid;
 
-    public Project(int project_id, String project_name, int project_rate, int client_id)
+    public Project(int project_id, String project_name, int project_rate, int client_id, String total_tid)
     {
         this.project_id = project_id;
         this.project_name = project_name;
         this.project_rate = project_rate;
         this.client_id = client_id;
+        this.total_tid = total_tid;
     }
 
     public Project()
     {
     }
 
+    public String getTotal_tid() {
+        return total_tid;
+    }
+
+    public void setTotal_tid(String total_tid) {
+        this.total_tid = total_tid;
+    }
+
+    
+    
     public int getProject_id()
     {
         return project_id;
