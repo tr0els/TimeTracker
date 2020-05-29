1ce3872049829e48718492a0e5646061207ba74a
René Jørgensen <56912824+LeChampDK@users.noreply.github.com>
2020-05-29 11:05:58 +0200

    Update TaskModel.java

--- src/timetracker/GUI/Model/TaskModel.java
+++ src/timetracker/GUI/Model/TaskModel.java
@@ -25,7 +25,8 @@ import timetracker.DAL.DALException;
  * @author Brian Brandt, Kim Christensen, Troels Klein, René Jørgensen &
  * Charlotte Christensen
  */
-public class TaskModel implements Runnable {
+public class TaskModel implements Runnable
+{
 
     /**
      * Singleton opsætning af vores model. singleton gør at vores model ikke vil
@@ -33,71 +34,39 @@ public class TaskModel implements Runnable {
      */
     private static BLLManager bll;
     private static TaskModel model = null;
-    //private ObservableList<Client> allClients;
-    //private ObservableList<Project> allProjects;
-    //private ObservableList<User> allUsers;
     private ObservableList<TaskForDataView> taskForDataview;
 
-    public TaskModel() throws DALException, SQLException {
+    public TaskModel() throws DALException, SQLException
+    {
         bll = BLLManager.getInstance();
-        //allProjects = FXCollections.observableArrayList();
-        //allProjects.addAll(bll.getProjects());
-        //allClients = FXCollections.observableArrayList();
-        // allClients.addAll(bll.getClients());
-        // allUsers = FXCollections.observableArrayList();
-        //allUsers.addAll(bll.getUsers());
-//        taskById = FXCollections.observableArrayList();
-//        logsById = FXCollections.observableArrayList();
         taskForDataview = FXCollections.observableArrayList();
-
     }
 
     @Override
-    public void run() {
-        try {
+    public void run()
+    {
+        try
+        {
             model = new TaskModel();
-        } catch (DALException ex) {
+        } catch (DALException ex)
+        {
             Logger.getLogger(TaskModel.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
+        } catch (SQLException ex)
+        {
             Logger.getLogger(TaskModel.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
 
-    public static TaskModel getInstance() throws DALException, SQLException {
-        if (model == null) {
+    public static TaskModel getInstance() throws DALException, SQLException
+    {
+        if (model == null)
+        {
             model = new TaskModel();
         }
         return model;
     }
 
-//    /**
-//     * Sender det info fra MainControllerens "createProject" videre til DAL
-//     * laget
-//     *
-//     * @param clientID
-//     * @param projectName
-//     * @param hourlyPay
-//     * @throws DALException
-//     */
-//    public void createProject(int clientID, String projectName, int hourlyPay) throws DALException
-//    {
-//        bll.createProject(clientID, projectName, hourlyPay);
-//    }
-//
-//    /**
-//     * Sender det info fra MainControllerens "deleteProject" videre til DAL
-//     * laget
-//     *
-//     * @param clientID
-//     * @param projectName
-//     * @param hourlyPay
-//     * @throws DALException
-//     */
-//    public void deleteProject(int projectID) throws DALException
-//    {
-//        bll.deleteProject(projectID);
-//    }
     /**
      * Starter en ny task
      *
@@ -107,7 +76,8 @@ public class TaskModel implements Runnable {
      * @param person_id
      * @throws timetracker.DAL.DALException
      */
-    public void startTask(String task_name, boolean billable, int project_id, int person_id) throws DALException {
+    public void startTask(String task_name, boolean billable, int project_id, int person_id) throws DALException
+    {
         bll.startTask(task_name, billable, project_id, person_id);
     }
 
@@ -117,12 +87,14 @@ public class TaskModel implements Runnable {
      * @param person_id
      * @throws timetracker.DAL.DALException
      */
-    public void stopTask(int person_id) throws DALException {
+    public void stopTask(int person_id) throws DALException
+    {
         bll.stopTask(person_id);
 
     }
-    
-    public List<TaskGroup> getTasksGroupedByDate(int personId, String groupBy, boolean includeTaskParents, boolean includeTaskChildren) throws DALException {
+
+    public List<TaskGroup> getTasksGroupedByDate(int personId, String groupBy, boolean includeTaskParents, boolean includeTaskChildren) throws DALException
+    {
         return bll.getTasksGroupedByDate(personId, groupBy, includeTaskParents, includeTaskChildren);
     }
 
@@ -135,7 +107,8 @@ public class TaskModel implements Runnable {
      * @return
      * @throws timetracker.DAL.DALException
      */
-    public TreeMap<Task, List<Task>> getTaskbyIDs(int project_id, int person_id) throws DALException {
+    public TreeMap<Task, List<Task>> getTaskbyIDs(int project_id, int person_id) throws DALException
+    {
 //        taskById.clear();
 //        taskById.addAll(bll.getTaskbyIDs(project_id, person_id));
 //        return taskById;
@@ -144,165 +117,33 @@ public class TaskModel implements Runnable {
 
     /**
      * returnere en specifik task udfra task_id
+     *
      * @param task_id
      * @return
-     * @throws DALException 
+     * @throws DALException
      */
-    public Task getTaskbyID(int task_id) throws DALException {
+    public Task getTaskbyID(int task_id) throws DALException
+    {
         return bll.getTaskbyID(task_id);
     }
-    
+
     /**
      * Sender en task til db for at opdatere den.
+     *
      * @param task
-     * @throws DALException 
+     * @throws DALException
      */
-    public void updateTaskbyID(Task task) throws DALException{
+    public void updateTaskbyID(Task task) throws DALException
+    {
         bll.updateTaskbyID(task);
     }
-//    public ObservableList<Log> getLogsbyID(int task_id) throws DALException {
-//        logsById.clear();
-//        logsById.addAll(bll.getLogsbyID(task_id));
-//        return logsById;
-//    }
-//    /**
-//     * Sender det info fra MainControllerens "editProject" videre til DAL laget
-//     *
-//     * @param clientID
-//     * @param projectName
-//     * @param hourlyPay
-//     * @param projectID
-//     */
-//    public void editProject(int clientID, String projectName, int hourlyPay, int projectID)
-//    {
-//        bll.editProject(clientID, projectName, hourlyPay, projectID);
-//    }
-//
-//    /**
-//     * retunere en liste af alle projekter.
-//     *
-//     * @return
-//     * @throws DALException
-//     * @throws SQLException
-//     */
-//    public ObservableList<Project> getProjects() throws DALException, SQLException {
-//        allProjects.clear();
-//        allProjects.addAll(bll.getProjects());
-//        Comparator<Project> byName = (Project cl1, Project cl2) -> cl1.getProject_name().compareTo(cl2.getProject_name());
-//        allProjects.sort(byName);
-//
-//        return allProjects;
-//    }
-//
-//    public Project getProject(String projectName, int project_rate, int client_id)
-//    {
-//        return bll.getProject(projectName, project_rate, client_id);
-//    }
-//    /**DENNE METODE LIGGER INDE I CLIENTMODEL
-//     * Sender Client objekt ned til DAL laget som skal oprettes.
-//     *
-//     * @param client
-//     */
-//    public void createClient(Client client) {
-//        bll.createClient(client);
-//    }
-//    /**
-//     * Sender Client objekt ned til DAL laget som skal ændres.
-//     *
-//     * @param client
-//     */
-//    public void editClient(Client client)
-//    {
-//        bll.deleteClient(client);
-//    }
-//
-//    /**
-//     * Sender Client objekt ned til DAL laget som skal slettes.
-//     *
-//     * @param client
-//     */
-//    public void deleteClient(Client client)
-//    {
-//        bll.deleteClient(client);
-//    }
-//    /**
-//     * Returnerer en liste med alle Clients.
-//     *
-//     * @return
-//     * @throws DALException
-//     * @throws SQLException
-//     */
-//    public ObservableList<Client> getClients() throws DALException, SQLException
-//    {
-//        Comparator<Client> byName = (Client cl1, Client cl2) -> cl1.getClient_name().compareTo(cl2.getClient_name());
-//        allClients.sort(byName);
-//        return allClients;
-//    }
-//    /**
-//     * Sender User objekt ned til DAL laget som skal oprettes.
-//     *
-//     * @param client
-//     */
-//    public void createUser(User user)
-//    {
-//        bll.createUser(user);
-//    }
-//
-//    /**
-//     * Sender User objekt ned til DAL laget som skal ændres.
-//     *
-//     * @param client
-//     */
-//    public void editUser(User user)
-//    {
-//        bll.editUser(user);
-//    }
-//
-//    /**
-//     * Sender Client objekt ned til DAL laget som skal slettes.
-//     *
-//     * @param client
-//     */
-//    public void deleteUser(User user)
-//    {
-//        bll.deleteUser(user);
-//    }
-//
-//    /**
-//     * Henter listen af Users nede fra DAL laget
-//     *
-//     * @return
-//     * @throws DALException
-//     * @throws SQLException
-//     */
-//    public ObservableList<User> getUsers() throws DALException, SQLException
-//    {
-//        allUsers.clear();
-//        allUsers.addAll(bll.getUsers());
-//        Comparator<User> byName = (User cl1, User cl2) -> cl1.getName().compareTo(cl2.getName());
-//        allUsers.sort(byName);
-//        return allUsers;
-//    }
-//
-//    /**
-//     * Returnerer en liste med de forskellige Professions.
-//     *
-//     * @return
-//     * @throws DALException
-//     * @throws SQLException
-//     */
-//    public ObservableList<Profession> getProfessions() throws DALException, SQLException
-//    {
-//        allProfessions.addAll(bll.getProfessions());
-//        return allProfessions;
-//    }
- 
-     public ObservableList<TaskForDataView> getListOfTaskForDataView(Project project, User user, String fradato, String tildato, String monthStart, String monthEnd) throws DALException{
-     taskForDataview.clear();
-     taskForDataview.addAll(bll.getListOfTaskForDataView(project, user, fradato,tildato,monthStart,monthEnd));
-     return taskForDataview;
-     
-     
-     }
-    
+
+    public ObservableList<TaskForDataView> getListOfTaskForDataView(Project project, User user, String fradato, String tildato, String monthStart, String monthEnd) throws DALException
+    {
+        taskForDataview.clear();
+        taskForDataview.addAll(bll.getListOfTaskForDataView(project, user, fradato, tildato, monthStart, monthEnd));
+        return taskForDataview;
+
+    }
+
 }
